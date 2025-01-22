9a814adab083c7b613459d2df9786be82355fdd2
Luís Fonseca <luis@lhf.pt>
2025-01-22 21:11:53 +0200

    test: add and remove node

---
+++ tests/test_state.py
@@ -0,0 +1,26 @@
+from netemu.state import State
+import os
+import time
+
+def test_new_node():
+    state = State()
+    nid = state.new_node()
+
+    time.sleep(1)
+
+    assert nid == "n1"
+    assert nid in state.nodes
+
+    assert os.path.exists(f"/proc/{state.nodes[nid].proc.pid}")
+
+    parent_userns = os.readlink(f"/proc/{os.getpid()}/ns/user")
+    parent_netns = os.readlink(f"/proc/{os.getpid()}/ns/net")
+    userns = os.readlink(f"/proc/{state.nodes[nid].proc.pid}/ns/user")
+    netns = os.readlink(f"/proc/{state.nodes[nid].proc.pid}/ns/net")
+
+    assert userns == parent_userns
+    assert netns != parent_netns
+
+    state.close_nodes()
+
+    assert not os.path.exists(f"/proc/{state.nodes[nid].proc.pid}")
