f4e4901ac154104cef35245e67dbaae7cde685bc
Luís Fonseca <luis@lhf.pt>
2025-01-22 22:18:50 +0200

    test: execute in node

--- tests/test_state.py
+++ tests/test_state.py
@@ -21,3 +21,25 @@ def test_new_node():
     state.close_nodes()
 
     assert not os.path.exists(f"/proc/{state.nodes[nid].proc.pid}")
+
+def test_execute_no_node(capfd):
+    state = State()
+
+    state.execute("n1", ["sh"])
+
+    assert capfd.readouterr().out == "Node n1 does not exist\n"
+
+def test_execute(capfd):
+    state = State()
+    nid = state.new_node()
+
+    netns = os.readlink(f"/proc/{state.nodes[nid].proc.pid}/ns/net")
+
+    capfd.readouterr()
+
+    state.execute(nid, ["readlink", "/proc/self/ns/net"])
+
+    assert capfd.readouterr().out == f"{netns}\n"
+
+    state.close_nodes()
+    
